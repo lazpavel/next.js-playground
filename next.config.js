bb6817b38f8c61f846bebddff2fa2abe7473ad1c
Pavel Lazar <pavel@alazara.com>
2024-01-16 17:34:26 -0500

    chore: new playground

--- next.config.js
+++ next.config.js
@@ -1,20 +1,4 @@
 /** @type {import('next').NextConfig} */
-
-const nextConfig = {
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: '**.nasa.gov',
-        port: '',
-      },
-      {
-        protocol: 'https',
-        hostname: 'lazpavel-test-image-optimization.s3.us-west-2.amazonaws.com',
-        port: '',
-      }
-    ],
-  },
-}
+const nextConfig = {}
 
 module.exports = nextConfig
