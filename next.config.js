6e671348f85c88fa7e9d8e39b8bb1d9b8aa70d29
lazpavel <pavel@alazara.com>
2024-01-29 21:11:10 -0500

    playground

--- next.config.js
+++ next.config.js
@@ -1,4 +1,19 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'apod.nasa.gov',
+        port: '',
+      },
+      {
+        protocol: 'https',
+        hostname: '*.amplifyapp.com',
+        port: '',
+      }
+    ],
+  },
+}
 
 module.exports = nextConfig
