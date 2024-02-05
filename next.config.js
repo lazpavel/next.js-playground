1b89d737cd4d97997bc7cdafaaa75f4d46db700b
lazpavel <pavel@alazara.com>
2024-02-05 17:15:34 -0500

    chore: ISR playground

--- next.config.js
+++ next.config.js
@@ -1,5 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  // cacheHandler: require.resolve('/Users/lazpavel/Amazon/github/amplify-hosting-cache/dist/shared-key-value-storage.js'),
+  cacheMaxMemorySize: 0, // disable default in-memory caching
   images: {
     remotePatterns: [
       {
