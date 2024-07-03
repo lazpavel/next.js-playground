b58b2c86e5612839f5cf613d0588341793f0c271
Pavel Lazar <85319655+lazpavel@users.noreply.github.com>
2024-07-03 13:54:46 -0400

    chore: test with via header

--- next.config.js
+++ next.config.js
@@ -25,21 +25,35 @@ const nextConfig = {
   async redirects() {
     return [
       {
+        source: '/old-path/(.*)',
+        destination: '/new-path/$1',
+        permanent: true,
+      },
+      {
         source: '/post/:slug(\\d{1,})',
         destination: '/posts/:slug',
         permanent: true,
       },
       {
+        source: "/old-blog/:first(\\d{1,})/should-be-here/:second*",
+        destination: "/new-blog/:first/should-be-here/:second*",
+        permanent: false,
+      },
+      {
+        locale: false,
         source: '/old-blog/:first*/should-be-here/:second*',
         destination: '/new-blog/:first*/should-be-here/:second*',
         permanent: false,
       },
       {
+        basePath: false,
+        locale: false,
         source: '/redirect-me',
         destination: '/redirected',
         permanent: true,
       },
       {
+        basePath: false,
         source: '/redirect-posts/:id',
         destination: '/posts/:id',
         permanent: true,
@@ -48,6 +62,42 @@ const nextConfig = {
         permanent: true,
         source: '/full-redirect-rule/:path*',
         destination: '/:method/:path*',
+        has: [
+          {
+            type: 'query',
+            key: 'method',
+            value: '(?<method>create|update)',
+          },
+        ],
+        missing: [
+          {
+            type: 'host',
+            value: 'not-amplifyapp.com',
+          },
+          {
+            type: 'header',
+            key: 'x-amplify-optimized',
+            value: ':no-cache(no-cache)',
+          }
+        ],
+      },
+    ]
+  },
+
+  async headers() {
+    return [
+      {
+        source: '/full-rewrite-rule/:path*',
+        headers: [
+          {
+            key: 'x-custom-header',
+            value: 'custom value',
+          },
+          {
+            key: 'x-amplify-processed',
+            value: 'true',
+          }
+        ],
         has: [
           {
             type: 'query',
@@ -106,6 +156,10 @@ const nextConfig = {
             key: 'x-amplify-optimized',
             value: 'true',
           },
+          {
+            key: 'via',
+            value: 'something',
+          }
         ],
       },
     ]
